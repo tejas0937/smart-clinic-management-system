07d88879f8ef7247ceeda93b47684e03e640dabf
tejas0937 <tejsthigale0937@gmail.com>
2026-02-07 23:08:38 +0530

    Initial Folder setup

---
+++ backend/smart-clinic-api/src/main/java/com/smartcare/smart_clinic_api/SmartClinicApiApplication.java
@@ -0,0 +1,13 @@
+package com.smartcare.smart_clinic_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartClinicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartClinicApiApplication.class, args);
+	}
+
+}
